244345b96c7739bf34227e6ef2fba6c6fff19762
jaskaa74 <jaska.sidhu499@gmail.com>
2024-11-25 17:44:48 +0100

    Refactoring Prima Parte

--- bst_lib.h
+++ bst_lib.h
@@ -1,220 +1,40 @@
 #include <iostream>
 using namespace std;
 
-struct node {
+class Node {
+private:
     int data;
     int rip;
-    node* leftc;
-    node* rightc;
+    Node* leftc;
+    Node* rightc;
+public:
+    Node(int d) :
+        rip{ 1 }, data{ d }, rightc{ nullptr }, leftc{ nullptr } {};
 
-    node(int d) {
-        rip = 1;
-        data = d;
-        rightc = nullptr;
-        leftc = nullptr;
-    }
-};
-
-node* insert(node* root, int key) {
-    if (root == nullptr) {
-        return new node(key);
-    }
-
-    if (key == root->data) {
-        root->rip++;
-    }
-    else if (key < root->data) {
-        root->leftc = insert(root->leftc, key);
-    }
-    else {
-        root->rightc = insert(root->rightc, key);
-    }
-
-    return root;
-}
-
-node* insert2(node* r, int key) {
-    if (r == nullptr) {
-        return new node(key);
-    }
-
-    node* current = r;
-    node* parent = nullptr;
-
-    while (current != nullptr) {
-        parent = current;
-        if (key == current->data) {
-            current->rip++;
-            return r;
+    Node* insertR(int k) {
+        if (this == nullptr) {
+            return new Node(k);
         }
-        else if (key < current->data) {
-            current = current->leftc;
+
+        if (k == this->data) {
+            this->rip++;
+        }
+        else if (k < this->data) {
+            this->leftc = this->leftc->insertR(k);
         }
         else {
-            current = current->rightc;
+            this->rightc = this->rightc->insertR(k);
         }
+
+        return this;
     }
 
-    if (key < parent->data) {
-        parent->leftc = new node(key);
+    void inOrder() {
+        if (this == nullptr) return;
+
+        this->leftc->inOrder();
+        cout << this->data << " ";
+        this->rightc->inOrder();
+
     }
-    else {
-        parent->rightc = new node(key);
-    }
-
-    return r;
-}
-
-void a(int i) {
-    cout << "a";
-}
-
-void pre_order(node* r) {
-    if (r == nullptr) return;
-
-    cout << r->data << " ";
-
-    pre_order(r->leftc);
-    pre_order(r->rightc);
-
-}
-
-void in_order(node* r) {
-    if (r == nullptr) return;
-
-    in_order(r->leftc);
-    cout << r->data << " ";
-    in_order(r->rightc);
-
-}
-
-void post_order(node* r) {
-    if (r == nullptr) return;
-
-    pre_order(r->leftc);
-    pre_order(r->rightc);
-
-    cout << r->data << " ";
-}
-
-node* DeleteL(node* r, int key) {            //per foglie
-    if (r == nullptr) {
-        return nullptr;
-    }
-
-    if (key == r->data) {
-        delete r;
-        return nullptr;
-    }
-    else if (key < r->data) {
-        r->leftc = DeleteL(r->leftc, key);
-    }
-    else {
-        r->rightc = DeleteL(r->rightc, key);
-    }
-
-    return r;
-}
-
-node* DeleteC(node* r, int key) {            //per nodi con 1 figlio
-    if (r == nullptr) {
-        return nullptr;
-    }
-
-    if (key < r->data) {
-        r->leftc = DeleteC(r->leftc, key);
-    }
-    else if (key > r->data) {
-        r->rightc = DeleteC(r->rightc, key);
-    }
-    else if (r->leftc == nullptr) {
-        node* temp = r;
-        r = r->rightc;
-        delete temp;
-    }
-    else if (r->rightc == nullptr) {
-        node* temp = r;
-        r = r->leftc;
-        delete temp;
-    }
-
-    return r;
-}
-
-node* DeleteN(node* r, int key) {            //per nodi con 2 figli
-    if (r == nullptr) {
-        return nullptr;
-    }
-
-    if (key < r->data) {
-        r->leftc = DeleteN(r->leftc, key);
-    }
-    else if (key > r->data) {
-        r->rightc = DeleteN(r->rightc, key);
-    }
-    else {
-        if (r->rightc != nullptr && r->leftc != nullptr) {
-            node* parent = r;
-            node* temp = r->rightc;
-
-            while (temp->leftc != nullptr) {
-                parent = temp;
-                temp = temp->leftc;
-            }
-
-            r->data = temp->data;
-
-            if (parent->leftc == temp) {
-                parent->leftc = temp->rightc;
-            }
-            else {
-                parent->rightc = temp->rightc;
-            }
-
-            delete temp;
-        }
-    }
-
-    return r;
-}
-
-int height(node* root) {
-    if (root == NULL) return 0;
-
-    int l = height(root->leftc);
-    int r = height(root->rightc);
-
-    return max(l, r) + 1;
-}
-
-node* find(node* root, int key) {
-    if (root == nullptr) {
-        cout << "il numero non c'è" << endl;
-        return nullptr;
-    }
-
-    if (key == root->data) {
-        cout << "il numero si trova: " << root << " ed è stato inserito " << root->rip << " volte" << endl;
-        return root;
-    }
-    else if (key < root->data) {
-        return find(root->leftc, key);
-    }
-    else {
-        return find(root->rightc, key);
-    }
-
-    return root;
-}
-
-bool isBST(node* r, int min = INT_MIN, int max = INT_MAX) {
-    if (r == nullptr) {
-        return true;
-    }
-
-    if (r->data < min || r->data > max) {
-        return false;
-    }
-
-    return isBST(r->leftc, min, r->data) && isBST(r->rightc, r->data, max);
-}
+};
